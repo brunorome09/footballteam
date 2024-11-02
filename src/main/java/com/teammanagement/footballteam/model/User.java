8dd87f857e362d708eec4961063602d9ff46b02b
brunorome09 <brunoromero860@gmail.com>
2024-11-02 15:05:36 -0300

    User add

--- src/main/java/com/teammanagement/footballteam/model/User.java
+++ src/main/java/com/teammanagement/footballteam/model/User.java
@@ -1,15 +1,16 @@
 package com.teammanagement.footballteam.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
 
 @Entity
+@Table(name = "usuarios")
 public class User {
     @Id
-    private UUID user_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
 
     private String name;
     private String password;
@@ -17,7 +18,6 @@ public class User {
     public User(String name, String password) {
         this.name = name;
         this.password = password;
-        this.user_id = UUID.randomUUID();
     }
 
     public String getName() {
@@ -36,7 +36,7 @@ public class User {
         this.password = password;
     }
 
-    public UUID getTeamid() {
+    public Long getTeamid() {
         return user_id;
     }
 }
